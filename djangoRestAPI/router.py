c5f55a5b0cacf59a6dd76b5ce9509869833e582d
EmmanuelAddi <emmanuelbett82108@gmail.com>
2022-11-19 13:16:48 +0300

    Django Rest api with Viewsets, Routers and Serializers

--- djangoRestAPI/router.py
+++ djangoRestAPI/router.py
@@ -2,7 +2,7 @@ from snippets.api.viewsets import SnippetViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('snippets', SnippetViewSet, basename='snippet')
+router.register('snippets', SnippetViewSet, basename='JKUAT CU')
 
 #for  url in router.urls:
 #    print(url, '\n')
--- snippets/urls.py
+++ snippets/urls.py
@@ -2,8 +2,8 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'snippets'
+app_name = 'JKUAT CU'
 urlpatterns = [
-    path('', views.SnippetListView.as_view(), name='list'),
+    path('', views.SnippetListView.as_view(), name='Ministries'),
     path('<int:pk>/', views.SnippetDetailView.as_view(), name='detail')
 ]
